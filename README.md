ce6b9f366e4b510423b3091b0ce12102b561d9a5
Stephen Okita <87273292+TheArctesian@users.noreply.github.com>
2021-12-11 13:10:15 +0800

    init next js

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CryptoChess.io
+
+A Dapp that allows users to play chess.
+
+If you would like to contribute to the development or the team please message me
+
+discord : @the_arctesian#8968
+telegram : @TheArctesian
---
+++ Web/next-web/pages/index.js
@@ -0,0 +1,12 @@
+function HomePage() {
+    return; <
+    div >
+        <
+        h1 > home < /h1> <
+        p >
+        lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec euismod <
+        /p> <
+        /div>;
+}
+
+export default HomePage;
