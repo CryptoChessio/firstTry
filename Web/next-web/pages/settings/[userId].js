468cbcd55d594f84c1ec73b00e195ada7e83215f
Stephen Okita <87273292+TheArctesian@users.noreply.github.com>
2021-12-11 23:46:35 +0800

    nav routing

---
+++ Web/next-web/pages/settings/[userId].js
@@ -0,0 +1,19 @@
+import { useRouter } from 'next/router'
+
+function settings() {
+  const router = useRouter();
+  console.log(router.query.userId); //getting the user ID
+
+  const UserId = router.query.userID;
+
+  // send req for user info and settings
+
+  return <div>
+    <h1>settings</h1>
+    <p>
+      lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod
+    </p>
+  </div>
+}
+
+export default settings;
---
+++ Web/next-web/pages/settings/index.js
@@ -0,0 +1,8 @@
+function settings() {
+  return <div>
+    <h1>Please log in</h1>
+    <button>Login with metaMask </button>
+  </div>
+}
+
+export default settings;
