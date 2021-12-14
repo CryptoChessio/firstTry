d353724d4ec079fa558becc00e2d4c90a238f961
Stephen Okita <87273292+TheArctesian@users.noreply.github.com>
2021-12-14 11:04:11 +0800

    Update [userId].jsx

--- Web/next-web/pages/settings/[userId].jsx
+++ Web/next-web/pages/settings/[userId].jsx
@@ -10,10 +10,21 @@ function settings() {
 
   return (
     <div className="p-3">
-      <h1>settings</h1>
-      <p>
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod
-      </p>
+      <h1>User</h1>
+      <div>
+      <div>
+        <p>UserName</p>
+        <form onSubmit={changeUserName()}>
+          <label>{user.isLog ? { userName } : "not logged in"}</label>
+          <input placeholder={userName}></input>
+          <button>Change Username</button>
+        </form>
+      </div>
+      <div>
+        <p>UserProfile</p>
+          <label><img src="{userId}/profileImage"/></label>
+      </div>
+      </div>
     </div>
   );
 }
