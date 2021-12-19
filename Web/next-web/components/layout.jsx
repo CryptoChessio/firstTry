5326da92d089cfdc0559fdf727003ca8e1bb6f43
Stephen Okita <87273292+TheArctesian@users.noreply.github.com>
2021-12-19 16:39:34 +0800

    some styling

--- Web/next-web/components/layout.jsx
+++ Web/next-web/components/layout.jsx
@@ -48,22 +48,22 @@ export default function Layout({ children }) {
       <nav className="flex">
         <div className="flex-1 m-5 bg-slate-900">
           <ul className="list-none flex text-center items-center justify-center">
-            <li className="mx-5 border-solid border-2 border-indigo-600 rounded p-2">
+            <li className="mx-5 border-solid border-2 border-indigo-600 rounded p-3 duration-700 ease-in-out  hover:border-indigo-200">
               <Link href="/">
                 <a>Home</a>
               </Link>
             </li>
-            <li className="mx-5 border-solid border-2 border-indigo-600 rounded p-2">
+            <li className="mx-5 border-solid border-2 border-indigo-600 rounded p-3 duration-700 ease-in-out  hover:border-indigo-200">
               <Link href="/play">
                 <a>Play</a>
               </Link>
             </li>
-            <li className="mx-5 border-solid border-2 border-indigo-600 rounded p-2">
+            <li className="mx-5 border-solid border-2 border-indigo-600 rounded p-3 duration-700 ease-in-out  hover:border-indigo-200">
               <Link href="/leaderboard">
                 <a>Leaderboard</a>
               </Link>
             </li>
-            <li className="mx-5 border-solid border-2 border-indigo-600 rounded p-2">
+            <li className="mx-5 border-solid border-2 border-indigo-600 rounded p-3 duration-700 ease-in-out  hover:border-indigo-200">
               <Link href="/about">
                 <a>About</a>
               </Link>
@@ -81,7 +81,7 @@ export default function Layout({ children }) {
               <Link href="/settings/{userID}">
                 <a>
                   <Image
-                    className="fill-white"
+                    className="fill-white duration-700 ease-in-out  hover:rotate-180"
                     alt="settings"
                     width={50}
                     height={50}
