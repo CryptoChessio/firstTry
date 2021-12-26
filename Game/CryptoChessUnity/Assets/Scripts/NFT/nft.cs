db1157e524d314b80cc416cbb5d55fbeb2ab6b6b
TheArctesian <stephen.d.okita@gmail.com>
2021-12-26 14:59:14 +0800

    x

--- Game/CryptoChessUnity/Assets/Scripts/NFT/nft.cs
+++ Game/CryptoChessUnity/Assets/Scripts/NFT/nft.cs
@@ -1,23 +1,27 @@
 using System.Collections;
-using System.Numerics;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ERC1155balanceOfBlueSphere : MonoBehaviour
 {
     public GameObject NFT;
+
     private BigInteger balanceOf;
+
     async void Start()
     {
         string chain = "polygon";
         string network = "testnet";
         string contract = "0x2953399124f0cbb46d2cbacd8a89cf0599974963";
         string account = PlayerPrefs.GetString("Account");
-        string tokenId = "34052697030098104870503300278835492448340531213250745283529521091668804108289";
+        string tokenId =
+            "34052697030098104870503300278835492448340531213250745283529521091668804108289";
 
-        balanceOf = await ERC1155.BalanceOf(chain, network, contract, account, tokenId);
-        print(balanceOf);
+        balanceOf =
+            await ERC1155.BalanceOf(chain, network, contract, account, tokenId);
+        print (balanceOf);
     }
 
     public void Selection()
@@ -26,9 +30,13 @@ public class ERC1155balanceOfBlueSphere : MonoBehaviour
         {
             if (NFT.activeInHierarchy == false)
             {
-                NFT.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+                NFT
+                    .GetComponent<Renderer>()
+                    .material
+                    .SetColor("_Color", Color.blue);
                 NFT.SetActive(true);
-            } else
+            }
+            else
             {
                 return;
             }
@@ -40,7 +48,8 @@ public class ERC1155balanceOfBlueSphere : MonoBehaviour
         if (NFT.activeInHierarchy == true)
         {
             NFT.SetActive(false);
-        } else
+        }
+        else
         {
             return;
         }
--- Web/contracts/WinLose.sol
+++ Web/contracts/WinLose.sol
@@ -1,14 +1,14 @@
 pragma solidity ^0.8.9;
 
 contract WinLose {
-    uint public playerCount = 0;
-    mapping (address => Player) public players;
 
     struct Player {
-        unit256 address;
         int team;
         bool moneyAdded;
     }
+
+    uint public playerCount = 0;
+    mapping (address => Player) public players;
 
     function addPlayer(string memory address, int team, bool moneyAdded){
         players[msg.sender] = Player(msg.sender, address, team, moneyAdded);
@@ -18,4 +18,11 @@ contract WinLose {
         playerCount +=1;
         }
     }
+
+    function getCandidate(uint id) external view returns (string memory name, uint voteCount) {
+  name = candidateLookup[id].name;
+  voteCount = candidateLookup[id].voteCount;
+  }
 }
+
+
